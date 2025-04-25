0b02d9b52d9249787b1c0a1d9cd185bb3152520e
⚡ Sigui <siguici@proton.me>
2025-04-26 00:42:52 +0100

    ✨ Add `silament:field` command

---
+++ src/Commands/MakeCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sikessem\Filament\Commands;
+
+use Illuminate\Console\Concerns\CreatesMatchingTest;
+use Illuminate\Console\GeneratorCommand;
+
+abstract class MakeCommand extends GeneratorCommand
+{
+    use CreatesMatchingTest;
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     */
+    protected function resolveStubPath($stub): string
+    {
+        $stub = trim($stub, '/');
+
+        return file_exists($customPath = $this->laravel->basePath($stub))
+            ? $customPath
+            : base_path($stub);
+    }
+
+    /**
+     * Get the desired class name from the input.
+     *
+     * @return string
+     */
+    protected function getNameInput()
+    {
+        $name = str(parent::getNameInput())->studly();
+
+        if (! $name->endsWith($this->type)) {
+            $name = $name->append(ucfirst($this->type));
+        }
+
+        return $name->toString();
+    }
+}
---
+++ src/Commands/MakeFieldCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Sikessem\Filament\Commands;
+
+use Symfony\Component\Console\Input\InputArgument;
+
+class MakeFieldCommand extends MakeCommand
+{
+    protected $name = 'silament:field';
+
+    protected $signature = 'silament:field {name} {component} {column} {label?} {--f|force}';
+
+    protected $description = 'Create a new field';
+
+    protected $type = 'field';
+
+    protected function getStub()
+    {
+        return $this->resolveStubPath('/stubs/field.stub');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the '.strtolower($this->type)],
+            ['component', InputArgument::REQUIRED, 'The form component of the '.strtolower($this->type)],
+            ['column', InputArgument::REQUIRED, 'The table column of the '.strtolower($this->type)],
+            ['label', InputArgument::OPTIONAL, 'The label of the '.strtolower($this->type)],
+        ];
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildClass($name)
+    {
+        $stub = parent::buildClass($name);
+
+        $this
+            ->replaceArgument($stub, 'component')
+            ->replaceArgument($stub, 'column')
+            ->replaceArgument($stub, 'name')
+            ->replaceArgument($stub, 'label');
+
+        return $stub;
+    }
+
+    protected function replaceArgument(string &$stub, string $name, string $default = 'null'): static
+    {
+        $subject = $name === 'name' ? str($this->getNameInput())->beforeLast(ucfirst($this->type))->snake() : str($this->argument($name))->trim();
+
+        if ($subject->isEmpty()) {
+            $subject = $default;
+        } elseif (in_array($name, ['name', 'label'])) {
+            $subject = $subject->replace(['\\', '\''], ['\\\\', '\\\''])->wrap('\'');
+        }
+
+        $stub = str_replace(['Dummy'.ucfirst($name), '{{ '.$name.' }}', '{{'.$name.'}}'], $subject, $stub);
+
+        return $this;
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\\Fields';
+    }
+}
